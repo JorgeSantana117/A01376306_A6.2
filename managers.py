85008d1109871d74a4c9831ca064a3542731edb3
Jorge Santana <jorge.santana11@outlook.com>
2026-02-21 19:38:26 -0600

    feat: add hotelmanager and its methods

--- managers.py
+++ managers.py
@@ -1,5 +1,6 @@
 import json
 import os
+from .models import Hotel, Customer, Reservation
 
 class BaseManager:
     def __init__(self, filename):
@@ -19,3 +20,49 @@ class BaseManager:
     def _save(self, data):
         with open(self.filename, 'w') as f:
             json.dump(data, f, indent=4)
+
+class HotelManager(BaseManager):
+    def create_hotel(self, hotel_id, name, location, rooms):
+        data = self._load()
+        data.append(Hotel(hotel_id, name, location, rooms).to_dict())
+        self._save(data)
+
+    def delete_hotel(self, hotel_id):
+        data = [h for h in self._load() if h['hotel_id'] != hotel_id]
+        self._save(data)
+
+    def display_hotel(self, hotel_id):
+        hotels = self._load()
+        for h in hotels:
+            if h['hotel_id'] == hotel_id:
+                print(f"Hotel Info: {h}")
+                return h
+        print("Hotel not found.")
+        return None
+
+    def modify_hotel(self, hotel_id, **kwargs):
+        data = self._load()
+        for h in data:
+            if h['hotel_id'] == hotel_id:
+                h.update(kwargs)
+        self._save(data)
+
+    def reserve_room(self, hotel_id):
+        data = self._load()
+        for h in data:
+            if h['hotel_id'] == hotel_id:
+                if h['occupied_rooms'] < h['total_rooms']:
+                    h['occupied_rooms'] += 1
+                    self._save(data)
+                    return True
+        return False
+
+    def cancel_room_reservation(self, hotel_id):
+        data = self._load()
+        for h in data:
+            if h['hotel_id'] == hotel_id:
+                if h['occupied_rooms'] > 0:
+                    h['occupied_rooms'] -= 1
+                    self._save(data)
+                    return True
+        return False
