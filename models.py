dc92f0b72c7690a3f109693e07c5a92a5ce8873b
Jorge Santana <jorge.santana11@outlook.com>
2026-02-21 19:26:44 -0600

    feat: add hotel, customer and reservation classes

---
+++ models.py
@@ -0,0 +1,28 @@
+class Hotel:
+    def __init__(self, hotel_id, name, location, total_rooms):
+        self.hotel_id = hotel_id
+        self.name = name
+        self.location = location
+        self.total_rooms = total_rooms
+        self.occupied_rooms = 0
+
+    def to_dict(self):
+        return self.__dict__
+
+class Customer:
+    def __init__(self, customer_id, name, email):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+
+    def to_dict(self):
+        return self.__dict__
+
+class Reservation:
+    def __init__(self, reservation_id, customer_id, hotel_id):
+        self.reservation_id = reservation_id
+        self.customer_id = customer_id
+        self.hotel_id = hotel_id
+
+    def to_dict(self):
+        return self.__dict__
